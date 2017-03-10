c711a155c4c6b941b588e50eadefeef55f6e8b90
WHMCS RED <github@whmcs.red>
2017-03-10 15:22:29 -0300

    0.2
    Atualização de funções existente, criação de bloqueio de aviso caso o administrador que esteja logado na conta do cliente, alerta de login para área administrativa, criação de widget para saber sobre atualizações do hook.

--- login_avisa.php
+++ login_avisa.php
@@ -2,20 +2,22 @@
 //Desenvolvido por Luciano Zanita :: http://whmcs.red
 //Laravel DataBase
 use WHMCS\Database\Capsule;
-
 //Bloqueia o acesso direto ao arquivo
 if (!defined("WHMCS")){
 	die("Acesso restrito!");
 }
 //Função do Módulo
-function login_avisa($vars){
+function login_avisa_cliente($vars){
+
 	//Titulo do E-mail (nome da mensagem)
-	$mensagem = "Nome unico do e-mail";
+	$mensagem = "Aviso de Login";
 	//usuário do administrador
-	$admin = "usuario";
+	$adminsys = "lucianozanita";
+
 	//////////////////////////////////////////////////////
 	////////////NÃO MODIFICAR A PARTIR DAQUI//////////////
 	//////////////////////////////////////////////////////
+
 	//ID do usuário que esta logando
 	$id = $vars['userid'];
 	//Query para encontrar usuário
@@ -24,21 +26,58 @@ function login_avisa($vars){
 	foreach ($query as $cliente) {
     	$primeiro_nome = $cliente->firstname;
     	$segundo_nome = $cliente->lastname;
-
 	}
 	//Usuario do administrador
-	$administrador = $admin;
+	$administrador = $adminsys;
 	//ID do Cliente
 	$valores["id"] = $id;
 	//Email a ser enviado (nome dele)
 	$valores["messagename"] = $mensagem;
 	//Comando a ser executado na função
 	$comando = "sendemail";
-	//Executa o envio para a API Local
-	$executar = localAPI($comando, $valores, $administrador);
+	//Verifica se não é o Admin que logou na conta do cliente
+	if($_SESSION['adminid']==''){
+			//caso não tenha sido ele executa o comando.
+		//Executa o envio para a API Local
+		$executar = localAPI($comando, $valores, $administrador);
+	}
+	else{
+		//Caso tenha sido ele não executa nada aqui.
+	}
+
 }
+
+//Função do Módulo
+function login_avisa_admin($vars){
+
+	//Titulo do E-mail (nome da mensagem)
+	$mensagem = "Aviso de Login";
+	//usuário do administrador
+	$adminsys = "lucianozanita";
+
+	//////////////////////////////////////////////////////
+	////////////NÃO MODIFICAR A PARTIR DAQUI//////////////
+	//////////////////////////////////////////////////////
+
+	//Query para encontrar usuário
+	$query = Capsule::table('tbladmins')->WHERE('id', $vars['adminid'])->get();
+	//Obtendo informações agora do usuário
+	foreach ($query as $admin) {
+		$usuarioadmin = $admin->username;
+    	$primeiro_nome = $admin->firstname;
+    	$segundo_nome = $admin->lastname;
+	}
+	//Email a ser enviado (nome dele)
+	$valores["messagename"] = $mensagemadmin;
+	//Comando a ser executado na função
+	$comando = "SendAdminEmail";
+	//Executa o envio para a API Local
+	$executar = localAPI($comando, $valores, $adminsys);
+}
+
 //Strings para o E-mail
 function login_stringsemail($vars){
+
 	$loginstring = array();
 	//Obtendo informações do usuario que esta tentando logar
 	$loginstring['ip'] = getenv("REMOTE_ADDR");
@@ -46,10 +85,87 @@ function login_stringsemail($vars){
 	//Pegar a data e horário atual
 	$loginstring['data_atual'] = date("d/m/Y");
 	$loginstring['horario'] = date("H:m");
+
 	//Retorna valores
 	return $loginstring;
+
 }
-//Adicionar Função ao Cliente logar do WHMCS
-add_hook("ClientLogin",1,"login_avisa");
+
+
+//Adicionando Widget ao Admin Dashboard
+add_hook('AdminHomeWidgets', 1, function() {
+    return new LoginAvisa();
+});
+
+//Montagem do Wideget
+class LoginAvisa extends \WHMCS\Module\AbstractWidget
+{
+    protected $title = 'WHMCS.RED - Login Avisa';
+    protected $description = '';
+    protected $weight = 150;
+    protected $columns = 1;
+    protected $cache = false;
+    protected $cacheExpiry = 120;
+    protected $requiredPermission = '';
+
+    public function getData()
+    {
+        return array();
+    }
+
+    public function generateOutput($data)
+    {
+
+ $versao = "0.1";
+ $versaodisponivel = file_get_contents("http://whmcs.red/versao/loginavisa.txt");
+
+$codigo = '<div class="icon-stats">
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="item">
+                <div class="icon-holder text-center color-orange">
+                        <i class="fa fa-wrench"></i>
+                </div>
+                <div class="data">
+                    <div class="note">
+                        <a href="clients.php?status=Active">Versão Atual</a>
+                    </div>
+                    <div class="number">
+                            <span class="color-orange">'.$versao.'</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="item">
+                <div class="icon-holder text-center color-green">
+                    <i class="fa fa-wrench"></i>
+                </div>
+                <div class="data">
+                    <div class="note">
+                        Versão Disponível
+                    </div>
+                    <div class="number">
+                        <span class="color-green">'.$versaodisponivel.'</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>';
+ //Checa As versões
+ if($versao==$versaodisponivel){} else{ $codigo .= '<center><a href="http://whmcs.red/hook-login-avisa/" target="_new" class="btn btn-danger"><i class="fa fa-download" aria-hidden="true"></i> Baixar Atualizações</a></center><br/>'; }
+
+ 		//retorna o codigo
+        return $codigo;
+    }
+}
+
+
+//Adicionar Função ao Cliente e ou Admin logar do WHMCS
+add_hook("ClientLogin",1,"login_avisa_cliente");
+add_hook("AdminLogin",1,"login_avisa_admin");
+
 //Armazena as Strings para o email
 add_hook("EmailPreSend",1,"login_stringsemail");
+
